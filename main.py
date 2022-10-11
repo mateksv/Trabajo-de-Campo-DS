fffa43c0b738d9dcc90604b3c16f841d7e36e546
Mat-SV <sanchezviamonte01@gmail.com>
2022-10-11 10:15:16 -0300

    Create main.py

---
+++ main.py
@@ -0,0 +1,24 @@
+from Object import *
+
+
+valoresIndicadorCalidadCartaDePorte = [IndicadorCalidad('Enfermedades', 0, 1), 
+                                       IndicadorCalidad('Gluten', 30, 50), 
+                                       IndicadorCalidad('Ph', 7, 9)]
+
+arregloIndicadoresCalidad = [IndicadorCalidad('Enfermedades', 0, 1), 
+                             IndicadorCalidad('Gluten', 30, 50), 
+                             IndicadorCalidad('Ph', 7, 9)]
+
+contrato = Contrato('Contrato Campodonico 01')
+pesoNeto = 33
+
+
+s1 = Silo('silo1')
+s2 = Silo('silo2')
+s3 = Silo('silo3')
+s4 = Silo('silo4')
+s5 = Silo('silo5')
+
+arregloSilos = [s1,s2,s3,s4,s5]
+
+s1.setSilo(arregloSilos, pesoNeto, contrato, arregloIndicadoresCalidad, valoresIndicadorCalidadCartaDePorte)
